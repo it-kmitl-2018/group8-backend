5361377483f249f977fc25ededf5577e1c4c220f
pamai <pamai_5656@hotmail.com>
2018-03-05 14:21:21 +0700

    initial kotlin project

---
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/SpringBootExample.kt
@@ -0,0 +1,13 @@
+//Spring boot example, Will not use this file
+package th.ac.kmitl.it.soa.group8
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootExample {
+
+    fun main(args: Array<String>) {
+        SpringApplication.run(SpringBootExample::class.java, *args)
+    }
+}
