7d6ccce3b60a17275aedb63fd3d6bc69a8dd4f39
Unknown <58070114@kmitl.ac.th>
2018-03-05 17:51:58 +0700

    Change filename
    change filename SpringBootExample to EInvoiceApp and ETaxApplicationTests to EinvoiceAppTests

--- src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/EInvoiceAppTests.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/EInvoiceAppTests.kt
@@ -1,4 +1,4 @@
-package th.ac.kmitl.it.soa.group8
+package th.ac.kmitl.it.soa.group8.einvoice
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class ETaxApplicationTests {
+class EInvoiceAppTests {
 
 		//TODO
 }
