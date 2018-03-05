79173daf8d23dcf211ebdf31f59270e34cbdc1d0
Unknown <58070114@kmitl.ac.th>
2018-03-05 22:09:05 +0700

    Refactor code
    Refactor code follow convention

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/controller/HomeController.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/controller/HomeController.kt
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
-@RequestMapping("")
+@RequestMapping("/home")
 class HomeController {
 
-    @GetMapping("/")
-    fun home() : String {
+    @GetMapping("/hello")
+    fun home(): String {
         return "Hello, world"
     }
+
 }
