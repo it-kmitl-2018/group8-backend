0d2680000885ed38873f7f26c1a5f14ef2842cce
pamai <pamai_5656@hotmail.com>
2018-03-06 12:39:43 +0700

    Change model from ETaxInvoiceFormModel to ETaxInvoiceInfo

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/controller/ETaxInvoiceFormController.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/controller/ETaxInvoiceFormController.kt
@@ -2,16 +2,16 @@ package th.ac.kmitl.it.soa.group8.einvoice.controller
 
 import com.google.gson.Gson
 import org.springframework.web.bind.annotation.*
-import th.ac.kmitl.it.soa.group8.einvoice.model.ETaxInvoiceFormModel
+import th.ac.kmitl.it.soa.group8.einvoice.model.ETaxInvoiceInfo
 
 @RestController
 @RequestMapping("/etaxinvoiceform")
 class ETaxInvoiceFormController {
 
     @GetMapping("/")
-    fun getETaxInvoiceForm(): ETaxInvoiceFormModel {
-        var invoiceFormModel = ETaxInvoiceFormModel(1, "Montita", "ABC", "05/03/2661 16:48", "For test project",
-                "PP001", 123, "05/03/2561 16:48")
+    fun getETaxInvoiceForm(): ETaxInvoiceInfo {
+        var invoiceFormModel = ETaxInvoiceInfo("0", "0", "Example", "Unknown", "05/03/2661 12:30",
+                "Unknown", "0", "0", "06/03/2661 12:00", "", "")
         return invoiceFormModel
     }
 
@@ -19,7 +19,7 @@ class ETaxInvoiceFormController {
     @ResponseBody
     fun postETaxInvoiceForm(@RequestBody data: String): String {
         var gson = Gson()
-        var invoiceForm = gson.fromJson(data, ETaxInvoiceFormModel::class.java)
+        var invoiceForm = gson.fromJson(data, ETaxInvoiceInfo::class.java)
         return gson.toJson(invoiceForm)
     }
 
