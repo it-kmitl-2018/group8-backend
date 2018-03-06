8a6f959491d61e2a4076571324cb37a61fd7cdf8
pamai <pamai_5656@hotmail.com>
2018-03-06 22:25:35 +0700

    Rename function to get() and post()

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/controller/ETaxInvoiceFormController.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/controller/ETaxInvoiceFormController.kt
@@ -9,7 +9,7 @@ import th.ac.kmitl.it.soa.group8.einvoice.model.ETaxInvoiceInfo
 class ETaxInvoiceFormController {
 
     @GetMapping("/")
-    fun getETaxInvoiceForm(): ETaxInvoiceInfo {
+    fun get(): ETaxInvoiceInfo {
         var invoiceFormModel = ETaxInvoiceInfo("0", "0", "Example", "Unknown", "05/03/2661 12:30",
                 "Unknown", "0", "0", "06/03/2661 12:00", "", "")
         return invoiceFormModel
@@ -17,7 +17,7 @@ class ETaxInvoiceFormController {
 
     @PostMapping("/")
     @ResponseBody
-    fun postETaxInvoiceForm(@RequestBody data: String): String {
+    fun post(@RequestBody data: String): String {
         var gson = Gson()
         var invoiceForm = gson.fromJson(data, ETaxInvoiceInfo::class.java)
         return gson.toJson(invoiceForm)
