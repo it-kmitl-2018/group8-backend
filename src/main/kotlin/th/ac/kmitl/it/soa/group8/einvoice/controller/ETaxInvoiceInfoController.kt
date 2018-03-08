4db0dcc34ff5a5c8ff0423bb084e0ca1b07fe8c5
pamai <pamai_5656@hotmail.com>
2018-03-08 16:03:39 +0700

    Rename ETaxInvoiceInfo

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/controller/ETaxInvoiceInfoController.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/controller/ETaxInvoiceInfoController.kt
@@ -2,15 +2,15 @@ package th.ac.kmitl.it.soa.group8.einvoice.controller
 
 import com.google.gson.Gson
 import org.springframework.web.bind.annotation.*
-import th.ac.kmitl.it.soa.group8.einvoice.model.ETaxInvoiceInfo
+import th.ac.kmitl.it.soa.group8.einvoice.model.ETaxInvoice
 
 @RestController
 @RequestMapping("/etaxinvoice")
 class ETaxInvoiceInfoController {
 
     @GetMapping("/")
-    fun get(): ETaxInvoiceInfo {
-        var invoiceFormModel = ETaxInvoiceInfo("0", "0", "Example", "Unknown", "05/03/2661 12:30",
+    fun get(): ETaxInvoice {
+        var invoiceFormModel = ETaxInvoice("0", "0", "Example", "Unknown", "05/03/2661 12:30",
                 "Unknown", "0", "0", "06/03/2661 12:00", "", "")
         return invoiceFormModel
     }
@@ -19,7 +19,7 @@ class ETaxInvoiceInfoController {
     @ResponseBody
     fun post(@RequestBody data: String): String {
         var gson = Gson()
-        var invoiceForm = gson.fromJson(data, ETaxInvoiceInfo::class.java)
+        var invoiceForm = gson.fromJson(data, ETaxInvoice::class.java)
         return gson.toJson(invoiceForm)
     }
 
---
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/model/ETaxInvoice.kt
@@ -0,0 +1,13 @@
+package th.ac.kmitl.it.soa.group8.einvoice.model
+
+data class ETaxInvoice(val guidelineSpecifiedID: String,
+                       val documentId: String,
+                       val documentName: String,
+                       val documentType: String,
+                       val issueDateTime: String,
+                       val purpose: String,
+                       val purposeId: String,
+                       val documentGlobalId: String,
+                       val documentCreateDate: String,
+                       val remarkSubject: String,
+                       val remarkContent: String)
