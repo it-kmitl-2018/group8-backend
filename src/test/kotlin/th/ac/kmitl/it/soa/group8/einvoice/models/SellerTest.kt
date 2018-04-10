1a68a985b66fd7f7a1195f41cbb2f9076e68c67e
Ben <58070067@it.kmitl.ac.th>
2018-04-10 15:24:01 +0700

    add Seller test

---
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/models/SellerTest.kt
@@ -0,0 +1,33 @@
+package th.ac.kmitl.it.soa.group8.einvoice.models
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import th.ac.kmitl.it.soa.group8.einvoice.model.Seller
+
+class SellerTest {
+    @Test
+    fun sellerTest(){
+        val seller = Seller("3456","1234","Peter","5678",
+                        "Bob","customerService","abc@gmail.co.th",
+                        "022222222","A building","123 b road",
+                        "234 bangkok","rama 4","Prakanong","Prakanong",
+                        "234","112","890")
+        assertEquals("3456",seller.id)
+        assertEquals("1234",seller.globalID)
+        assertEquals("Peter",seller.name)
+        assertEquals("5678",seller.taxID)
+        assertEquals("Bob",seller.contactName)
+        assertEquals("customerService",seller.departmentName)
+        assertEquals("abc@gmail.co.th",seller.email)
+        assertEquals("022222222",seller.phoneNumber)
+        assertEquals("A building",seller.building)
+        assertEquals("123 b road",seller.addressOne)
+        assertEquals("234 bangkok",seller.addressTwo)
+        assertEquals("rama 4",seller.street)
+        assertEquals("Prakanong",seller.district)
+        assertEquals("Prakanong",seller.subDistrict)
+        assertEquals("234",seller.countryID)
+        assertEquals("112",seller.provinceID)
+        assertEquals("890",seller.houseNumber)
+    }
+}
