15156821893f3d746e6487f178779219d8a13da2
montitawi <58070114@kmitl.ac.th>
2018-04-15 17:14:41 +0700

    Update PostalTradeAddressModelTest
    Update PostalTradeAddressModelTest for test get XML String

--- src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/models/PostalTradeAddressModelTest.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/models/PostalTradeAddressModelTest.kt
@@ -1,24 +1,27 @@
 package th.ac.kmitl.it.soa.group8.einvoice.models
 
 import com.thoughtworks.selenium.SeleneseTestBase.assertEquals
+import junit.framework.Assert.assertNotNull
 import org.junit.Test
 import th.ac.kmitl.it.soa.group8.einvoice.model.PostalTradeAddressModel
+import java.io.StringWriter
+import javax.xml.bind.JAXBContext
 
 class PostalTradeAddressModelTest {
-    val postCode: String = "10310"
-    val building: String = "Chaiyaporn1"
-    val addressLineOne: String = "99/2546"
-    val addressLineTwo: String = "Room 602 6th Floor"
-    val addressLineThree: String = "Kekki Ngam1"
-    val addressLineFour: String = "Kekki"
-    val addressLineFive: String = "3"
-    val street: String = "Chalongkung"
-    val district: String = "1017"
-    val subDistrict: String = "Latkrabang"
-    val countryID: String = "TH"
-    val provinceID: String = "10"
-    val houseNumber: String = "3/34"
-    val postalTradeAddressModel = PostalTradeAddressModel(
+    private val postCode: String = "10310"
+    private val building: String = "Chaiyaporn1"
+    private val addressLineOne: String = "99/2546"
+    private val addressLineTwo: String = "Room 602 6th Floor"
+    private val addressLineThree: String = "Kekki Ngam1"
+    private val addressLineFour: String = "Kekki"
+    private val addressLineFive: String = "3"
+    private val street: String = "Chalongkung"
+    private val district: String = "1017"
+    private val subDistrict: String = "Latkrabang"
+    private val countryID: String = "TH"
+    private val provinceID: String = "10"
+    private val houseNumber: String = "3/34"
+    private val postalTradeAddressModel = PostalTradeAddressModel(
             this.postCode,
             this.building,
             this.addressLineOne,
@@ -47,8 +50,18 @@ class PostalTradeAddressModelTest {
         assertEquals(this.district, postalTradeAddressModel.district)
         assertEquals(this.subDistrict, postalTradeAddressModel.subDistrict)
         assertEquals(this.countryID, postalTradeAddressModel.countryID)
-        assertEquals(this.provinceID, postalTradeAddressModel.provinceID);
-        assertEquals(this.houseNumber, postalTradeAddressModel.houseNumber);
+        assertEquals(this.provinceID, postalTradeAddressModel.provinceID)
+        assertEquals(this.houseNumber, postalTradeAddressModel.houseNumber)
+    }
+
+    @Test
+    fun testGetXMLString() {
+        val context = JAXBContext.newInstance(PostalTradeAddressModel::class.java)
+        val marshaller = context.createMarshaller()
+        val stringWriter = StringWriter()
+        marshaller.marshal(postalTradeAddressModel, stringWriter)
+        val xmlString = stringWriter.toString()
+        assertNotNull(xmlString)
     }
 
 }
