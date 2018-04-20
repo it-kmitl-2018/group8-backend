06936f65028d8c28f16d80b4641cf8e427f45b85
montitawi <58070114@kmitl.ac.th>
2018-04-21 01:06:33 +0700

    Update TradePartyModel
    Add Javax.xml for convert data to XML tag PayerTradeParty

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/model/TradePartyModel.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/einvoice/model/TradePartyModel.kt
@@ -1,7 +1,21 @@
 package th.ac.kmitl.it.soa.group8.einvoice.model
 
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlElement
+import javax.xml.bind.annotation.XmlRootElement
+
+@XmlRootElement(name = "ram:PayerTradeParty")
+@XmlAccessorType(XmlAccessType.FIELD)
 data class TradePartyModel (
+
+        @field:XmlElement(name = "ram:ID")
         val id: String = "",
+
+        @field:XmlElement(name = "ram:GlobalID")
         val globalID: String = "",
+
+        @field:XmlElement(name = "ram:Name")
         val name: String = ""
+
 )
