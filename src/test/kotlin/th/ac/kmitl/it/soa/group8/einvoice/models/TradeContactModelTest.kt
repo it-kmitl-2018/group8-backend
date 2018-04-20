5ab08ea5fd333752aa72edd4fa6bb2ad8b2225f9
montitawi <58070114@kmitl.ac.th>
2018-04-21 00:16:26 +0700

    Update TradeContactModelTest
    Update TradeContactModelTest for test get XML String

--- src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/models/TradeContactModelTest.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/models/TradeContactModelTest.kt
@@ -1,8 +1,11 @@
 package th.ac.kmitl.it.soa.group8.einvoice.models
 
 import com.thoughtworks.selenium.SeleneseTestBase.assertEquals
+import junit.framework.Assert.assertNotNull
 import org.junit.Test
 import th.ac.kmitl.it.soa.group8.einvoice.model.TradeContactModel
+import java.io.StringWriter
+import javax.xml.bind.JAXBContext
 
 class TradeContactModelTest {
     val personName: String = "Montita"
@@ -16,6 +19,17 @@ class TradeContactModelTest {
     fun testGetAllInfo() {
         assertEquals(this.personName, tradeContactModel.personName)
         assertEquals(this.departmentName, tradeContactModel.departmentName)
+
+    }
+
+    @Test
+    fun testGetXMLString() {
+        val context = JAXBContext.newInstance(TradeContactModel::class.java)
+        val marshaller = context.createMarshaller()
+        val stringWriter = StringWriter()
+        marshaller.marshal(tradeContactModel, stringWriter)
+        val xmlString = stringWriter.toString()
+        assertNotNull(xmlString)
 
     }
 }
