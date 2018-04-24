00cd4001ea34a398b3025e5d380dfcf5e013c294
montitawi <58070114@kmitl.ac.th>
2018-04-24 10:15:31 +0700

    Rename package from einvoice to e_tax_invoice

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/e_tax_invoice/model/IncludedNoteModel.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/e_tax_invoice/model/IncludedNoteModel.kt
@@ -1,4 +1,4 @@
-package th.ac.kmitl.it.soa.group8.einvoice.model
+package th.ac.kmitl.it.soa.group8.e_tax_invoice.model
 
 data class IncludedNoteModel (
     val subject: String = "",
--- src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/models/IncludedNoteModelTest.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/models/IncludedNoteModelTest.kt
@@ -4,7 +4,7 @@ import com.thoughtworks.selenium.SeleneseTestBase.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
-import th.ac.kmitl.it.soa.group8.einvoice.model.IncludedNoteModel
+import th.ac.kmitl.it.soa.group8.e_tax_invoice.model.IncludedNoteModel
 
 @RunWith(SpringJUnit4ClassRunner::class)
 class IncludedNoteModelTest {
