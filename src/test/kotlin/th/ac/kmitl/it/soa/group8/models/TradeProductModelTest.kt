2b74a66b8e845720922dc1b18d8104903fda79fb
montitawi <58070114@kmitl.ac.th>
2018-04-28 15:21:50 +0700

    Move TradeProductModelTest to package models

--- src/test/kotlin/th/ac/kmitl/it/soa/group8/models/TradeProductModelTest.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/models/TradeProductModelTest.kt
@@ -1,8 +1,8 @@
-package th.ac.kmitl.it.soa.group8.einvoice.models
+package th.ac.kmitl.it.soa.group8.models
 
 import com.thoughtworks.selenium.SeleneseTestBase.assertEquals
 import org.junit.Test
-import th.ac.kmitl.it.soa.group8.einvoice.model.TradeProductModel
+import th.ac.kmitl.it.soa.group8.model.TradeProductModel
 
 class TradeProductModelTest {
     val id: String = "01"
