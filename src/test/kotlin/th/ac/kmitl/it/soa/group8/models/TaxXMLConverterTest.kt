de60687270e56c48a2cd42b704940ae582b61ed0
montitawi <58070114@kmitl.ac.th>
2018-05-02 00:25:59 +0700

    Rename TaxRegistrationModelTest to TaxXMLConverterTest
    Rename TaxRegistrationModelTest to TaxXMLConverterTest

--- src/test/kotlin/th/ac/kmitl/it/soa/group8/models/TaxXMLConverterTest.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/models/TaxXMLConverterTest.kt
@@ -3,28 +3,28 @@ package th.ac.kmitl.it.soa.group8.models
 import com.thoughtworks.selenium.SeleneseTestBase.assertEquals
 import junit.framework.Assert.assertNotNull
 import org.junit.Test
-import th.ac.kmitl.it.soa.group8.model.TaxRegistrationModel
+import th.ac.kmitl.it.soa.group8.model.TaxXMLConverter
 import java.io.StringWriter
 import javax.xml.bind.JAXBContext
 
-class TaxRegistrationModelTest {
+class TaxXMLConverterTest {
     val taxID: String = "1234567890123"
-    val taxRegistration = TaxRegistrationModel(
+    val taxXMLConverter = TaxXMLConverter(
             this.taxID
     )
 
     @Test
     fun testGetAllInfo() {
-        assertEquals(this.taxID, taxRegistration.taxID)
+        assertEquals(this.taxID, taxXMLConverter.taxID)
 
     }
 
     @Test
     fun testGetXMLString() {
-        val context = JAXBContext.newInstance(TaxRegistrationModel::class.java)
+        val context = JAXBContext.newInstance(TaxXMLConverter::class.java)
         val marshaller = context.createMarshaller()
         val stringWriter = StringWriter()
-        marshaller.marshal(taxRegistration, stringWriter)
+        marshaller.marshal(taxXMLConverter, stringWriter)
         val xmlString = stringWriter.toString()
         assertNotNull(xmlString)
 
