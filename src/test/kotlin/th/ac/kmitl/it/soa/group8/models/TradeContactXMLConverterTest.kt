2c44110447194c4cd4d28a81e8f8dcde8e14c6ef
montitawi <58070114@kmitl.ac.th>
2018-05-03 18:26:15 +0700

    Rename class from TradeContactModelTest  to TradeContactXMLConverterTest

--- src/test/kotlin/th/ac/kmitl/it/soa/group8/models/TradeContactXMLConverterTest.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/models/TradeContactXMLConverterTest.kt
@@ -3,31 +3,31 @@ package th.ac.kmitl.it.soa.group8.models
 import com.thoughtworks.selenium.SeleneseTestBase.assertEquals
 import junit.framework.Assert.assertNotNull
 import org.junit.Test
-import th.ac.kmitl.it.soa.group8.model.TradeContactModel
+import th.ac.kmitl.it.soa.group8.model.TradeContactXMLConverter
 import java.io.StringWriter
 import javax.xml.bind.JAXBContext
 
-class TradeContactModelTest {
+class TradeContactXMLConverterTest {
     val personName: String = "Montita"
     val departmentName: String = "Backend"
-    val tradeContactModel = TradeContactModel(
+    val tradeContact = TradeContactXMLConverter(
             this.personName,
             this.departmentName
     )
 
     @Test
     fun testGetAllInfo() {
-        assertEquals(this.personName, tradeContactModel.personName)
-        assertEquals(this.departmentName, tradeContactModel.departmentName)
+        assertEquals(this.personName, tradeContact.personName)
+        assertEquals(this.departmentName, tradeContact.departmentName)
 
     }
 
     @Test
     fun testGetXMLString() {
-        val context = JAXBContext.newInstance(TradeContactModel::class.java)
+        val context = JAXBContext.newInstance(TradeContactXMLConverter::class.java)
         val marshaller = context.createMarshaller()
         val stringWriter = StringWriter()
-        marshaller.marshal(tradeContactModel, stringWriter)
+        marshaller.marshal(tradeContact, stringWriter)
         val xmlString = stringWriter.toString()
         assertNotNull(xmlString)
 
