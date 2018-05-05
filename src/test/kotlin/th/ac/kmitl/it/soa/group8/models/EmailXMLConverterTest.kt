da683c1f43677ad23f2cd16a05f0168d8aeb9bb8
montitawi <58070114@kmitl.ac.th>
2018-05-05 16:28:08 +0700

    Rename class from EmailURIUniversalModelTest to EmailXMLConverterTest

--- src/test/kotlin/th/ac/kmitl/it/soa/group8/models/EmailXMLConverterTest.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/models/EmailXMLConverterTest.kt
@@ -2,11 +2,11 @@ package th.ac.kmitl.it.soa.group8.models
 
 import com.thoughtworks.selenium.SeleneseTestBase.assertEquals
 import org.junit.Test
-import th.ac.kmitl.it.soa.group8.model.EmailURIUniversalModel
+import th.ac.kmitl.it.soa.group8.model.EmailXMLConverter
 
-class EmailURIUniversalModelTest {
+class EmailXMLConverterTest {
     val uriID: String = "58070114@kmitl.ac.th"
-    val emailURIUniversal = EmailURIUniversalModel(
+    val emailURIUniversal = EmailXMLConverter(
             this.uriID
     )
 
