54eacda73738282163f257aa478ce4b08941364e
montitawi <58070114@kmitl.ac.th>
2018-05-05 16:35:26 +0700

    Add Javax.xml for convert data to XML
    Add Javax.xml for convert data to XML tag EmailURIUniversalCommunication

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/model/EmailXMLConverter.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/model/EmailXMLConverter.kt
@@ -1,5 +1,14 @@
 package th.ac.kmitl.it.soa.group8.model
 
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlElement
+import javax.xml.bind.annotation.XmlRootElement
+
+@XmlRootElement(name = "ram:EmailURIUniversalCommunication")
+@XmlAccessorType(XmlAccessType.FIELD)
 data class EmailXMLConverter (
+
+    @field:XmlElement(name = "ram:URIID")
     val uriID: String = ""
 )
