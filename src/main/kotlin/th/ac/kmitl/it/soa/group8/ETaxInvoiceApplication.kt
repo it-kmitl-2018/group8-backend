ea81752e2d9630dcfa14d923f9e1119203113fe3
pamai <pamai_5656@hotmail.com>
2018-05-08 20:31:46 +0700

    Change CrossOrigin declare method and remove unused import

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/ETaxInvoiceApplication.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/ETaxInvoiceApplication.kt
@@ -4,11 +4,7 @@ package th.ac.kmitl.it.soa.group8
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -17,13 +13,4 @@ class ETaxInvoiceApplication
 
     fun main(args: Array<String>) {
         SpringApplication.run(ETaxInvoiceApplication::class.java, *args)
-    }
-
-    @Bean
-    fun corsConfigurer(): Any {
-        return object: WebMvcConfigurerAdapter() {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/api").allowedOrigins("http://localhost:8080")
-            }
-        }
     }
--- src/main/kotlin/th/ac/kmitl/it/soa/group8/controller/ETaxInvoiceInfoController.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/controller/ETaxInvoiceInfoController.kt
@@ -5,21 +5,22 @@ import org.springframework.web.bind.annotation.*
 import th.ac.kmitl.it.soa.group8.model.ETaxInvoice
 
 @RestController
-@RequestMapping("/etaxinvoice")
 class ETaxInvoiceInfoController {
 
-    @GetMapping("/")
+    @GetMapping("/etaxinvoice")
     fun get(): ETaxInvoice {
         var invoiceFormModel = ETaxInvoice("Veronika Ilithyia", "123 New york", "Radhika Seetha", "54345 Thailand", "Car part",
                 12345)
         return invoiceFormModel
     }
 
-    @PostMapping("/")
+    @CrossOrigin(origins = ["http://localhost:8080"])
+    @PostMapping("/etaxinvoice")
     @ResponseBody
     fun post(@RequestBody data: String): String {
         var gson = Gson()
         var invoiceForm = gson.fromJson(data, ETaxInvoice::class.java)
+        System.out.println(data) //Print Frontend data to Backend console
         return gson.toJson(invoiceForm)
     }
 
