927a52f5ee9b9d120e68008b69b69d82d61800c1
Ben5497 <58070067@it.kmitl.ac.th>
2018-05-10 21:35:24 +0700

    add JsonConverter and Jsonconverter test

---
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/converter/JsonConverter.kt
@@ -0,0 +1,30 @@
+package th.ac.kmitl.it.soa.group8.converter
+
+import java.io.File
+import java.io.IOException
+import com.fasterxml.jackson.core.JsonGenerationException
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.json.simple.parser.JSONParser
+import org.json.simple.JSONObject
+import th.ac.kmitl.it.soa.group8.model.Seller
+import java.io.FileReader
+
+open class JsonConverter (){
+    @Throws(JsonGenerationException::class, JsonMappingException::class, IOException::class)
+    fun convertObjectToJson(`object`: Any) {
+        val mapper = ObjectMapper()
+        mapper.writeValue(System.out, `object`)
+        val jsonString = mapper.writeValueAsString(`object`)
+        mapper.writeValue(File("tax_invoice.json"), `object`)
+
+    }
+
+    fun convertJsontoObject(text:String): String {
+        val parser =  JSONParser()
+        val ob = parser.parse(FileReader(text))
+        val jsonObject = ob as JSONObject
+        val person = ob.get("id").toString()
+        return person
+    }
+}
---
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/JsonConverterTest.kt
@@ -0,0 +1,37 @@
+package th.ac.kmitl.it.soa.group8
+
+
+import java.io.IOException
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import th.ac.kmitl.it.soa.group8.converter.JsonConverter
+import th.ac.kmitl.it.soa.group8.model.Seller
+
+
+
+
+class JsonConverterTest {
+    @Test
+    fun convertJsontoObject_ShouldReturnBob() {
+        val converter = JsonConverter()
+        val input_json = "tax_invoice.json"
+        val taxInvoice = Seller("Bob",
+                "5678",
+                "Peter",
+                "1234",
+                "0123",
+                "customerService","abc@gmail.com","0111111",
+                "a tower","1234 B road","4567 A road","a street",
+                "prakanong","prakanong","012","456","123")
+        assertEquals("Bob",converter.convertJsontoObject(input_json ))
+    }
+
+    @Test(expected = IOException::class)
+    fun IoException_ShouldReturnExceptionMessage() {
+        val converter = JsonConverter()
+        val input_json = "abc.json"
+        converter.convertJsontoObject(input_json )
+    }
+
+
+}
