23325344d763e054fc51cc2c296fb3c9252daafe
Ben5497 <58070067@it.kmitl.ac.th>
2018-05-10 21:09:06 +0700

    resolve travis error

--- src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/JsonConverterTest.kt
+++ src/test/kotlin/th/ac/kmitl/it/soa/group8/einvoice/JsonConverterTest.kt
@@ -3,13 +3,9 @@ package th.ac.kmitl.it.soa.group8.einvoice
 
 import java.io.IOException
 import junit.framework.TestCase.assertEquals
-import sun.plugin2.util.PojoUtil.toJson
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json
-import org.assertj.core.api.Assertions
 import org.junit.Test
 import th.ac.kmitl.it.soa.group8.einvoice.converter.JsonConverter
-import th.ac.kmitl.it.soa.group8.einvoice.model.ETaxInvoice
-import java.util.ArrayList
+import th.ac.kmitl.it.soa.group8.einvoice.model.Seller
 
 
 
@@ -19,12 +15,14 @@ class JsonConverterTest {
     fun convertJsontoObject_ShouldReturnBob() {
         val converter = JsonConverter()
         val input_json = "tax_invoice.json"
-        val taxInvoice = ETaxInvoice("Bob",
-                "123 a road",
+        val taxInvoice = Seller("Bob",
+                "5678",
                 "Peter",
-                "456 rama 9 road",
-                "product1",
-                1200)
+                "1234",
+                "0123",
+                "customerService","abc@gmail.com","0111111",
+                "a tower","1234 B road","4567 A road","a street",
+                "prakanong","prakanong","012","456","123")
         assertEquals("Bob",converter.convertJsontoObject(input_json ))
     }
 
