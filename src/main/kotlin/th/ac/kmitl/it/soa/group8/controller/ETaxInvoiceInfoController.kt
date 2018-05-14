b7f37c3a3687188410fe2146e234886e3b0a04dd
pamai <pamai_5656@hotmail.com>
2018-05-14 15:31:21 +0700

    Change mapping path and remove GetMapping

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/controller/ETaxInvoiceInfoController.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/controller/ETaxInvoiceInfoController.kt
@@ -2,25 +2,17 @@ package th.ac.kmitl.it.soa.group8.controller
 
 import com.google.gson.Gson
 import org.springframework.web.bind.annotation.*
-import th.ac.kmitl.it.soa.group8.model.ETaxInvoice
+import th.ac.kmitl.it.soa.group8.model.Seller
 
+@RequestMapping("/etax")
 @RestController
 class ETaxInvoiceInfoController {
 
-    @GetMapping("/etaxinvoice")
-    fun get(): ETaxInvoice {
-        var invoiceFormModel = ETaxInvoice("Veronika Ilithyia", "123 New york", "Radhika Seetha", "54345 Thailand", "Car part",
-                12345)
-        return invoiceFormModel
-    }
-
     @CrossOrigin(origins = ["http://localhost:8080"])
-    @PostMapping("/etaxinvoice")
-    @ResponseBody
+    @PostMapping("/invoice")
     fun post(@RequestBody data: String): String {
         var gson = Gson()
         var invoiceForm = gson.fromJson(data, ETaxInvoice::class.java)
-        System.out.println(data) //Print Frontend data to Backend console
         return gson.toJson(invoiceForm)
     }
 
