f83529729729d99db2f903dfc13cdc4a2d0e0813
pamai <pamai_5656@hotmail.com>
2018-05-14 15:33:05 +0700

    Revert "Change mapping path and remove GetMapping"
    This reverts commit b7f37c3a3687188410fe2146e234886e3b0a04dd.

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/controller/ETaxInvoiceInfoController.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/controller/ETaxInvoiceInfoController.kt
@@ -2,17 +2,25 @@ package th.ac.kmitl.it.soa.group8.controller
 
 import com.google.gson.Gson
 import org.springframework.web.bind.annotation.*
-import th.ac.kmitl.it.soa.group8.model.Seller
+import th.ac.kmitl.it.soa.group8.model.ETaxInvoice
 
-@RequestMapping("/etax")
 @RestController
 class ETaxInvoiceInfoController {
 
+    @GetMapping("/etaxinvoice")
+    fun get(): ETaxInvoice {
+        var invoiceFormModel = ETaxInvoice("Veronika Ilithyia", "123 New york", "Radhika Seetha", "54345 Thailand", "Car part",
+                12345)
+        return invoiceFormModel
+    }
+
     @CrossOrigin(origins = ["http://localhost:8080"])
-    @PostMapping("/invoice")
+    @PostMapping("/etaxinvoice")
+    @ResponseBody
     fun post(@RequestBody data: String): String {
         var gson = Gson()
         var invoiceForm = gson.fromJson(data, ETaxInvoice::class.java)
+        System.out.println(data) //Print Frontend data to Backend console
         return gson.toJson(invoiceForm)
     }
 
