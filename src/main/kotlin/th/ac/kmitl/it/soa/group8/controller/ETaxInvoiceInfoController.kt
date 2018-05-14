3cc841e41077df0eb98928f53595af45a7b7485c
pamai <pamai_5656@hotmail.com>
2018-05-14 18:21:57 +0700

    Change how to deserialize

--- src/main/kotlin/th/ac/kmitl/it/soa/group8/controller/ETaxInvoiceInfoController.kt
+++ src/main/kotlin/th/ac/kmitl/it/soa/group8/controller/ETaxInvoiceInfoController.kt
@@ -3,6 +3,7 @@ package th.ac.kmitl.it.soa.group8.controller
 import com.google.gson.Gson
 import org.springframework.web.bind.annotation.*
 import th.ac.kmitl.it.soa.group8.model.ETaxInvoice
+import th.ac.kmitl.it.soa.group8.model.Seller
 
 @RequestMapping("/etax")
 @RestController
@@ -11,11 +12,9 @@ class ETaxInvoiceInfoController {
     @CrossOrigin(origins = ["http://localhost:8080"])
     @PostMapping("/invoice")
     @ResponseBody
-    fun post(@RequestBody data: String): String {
-        var gson = Gson()
-        var invoiceForm = gson.fromJson(data, ETaxInvoice::class.java)
+    fun post(@RequestBody data: Seller): Seller {
         System.out.println(data) //Print Frontend data to Backend console
-        return gson.toJson(invoiceForm)
+        return data
     }
 
 }
